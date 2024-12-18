c796de3c63686417cc54934970990931819b5ff4
unknown <besmaazz56@gmail.com>
2024-12-18 23:46:17 +0100

    Rigister form update

--- RegisterForm.Designer.cs
+++ RegisterForm.Designer.cs
@@ -28,18 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(93, 67);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nom-utilisateur";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(214, 179);
+            button1.Name = "button1";
+            button1.Size = new Size(330, 56);
+            button1.TabIndex = 1;
+            button1.Text = "enregistrer";
+            button1.UseVisualStyleBackColor = true;
             // 
             // RegisterForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(742, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
             Name = "RegisterForm";
             Text = "RegisterForm";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
     }
 }
